ffa4b72eca6cec710302ee76a1392348f8b43640
VENKAT6912 <vibecoder18@gmail.com>
2025-09-30 15:02:59 +0530

    Initial commit of Codeforces problems

---
+++ 800-1000/StringTask.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class StringTask {       ///71A
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        while (n-- > 0) {
+            String s = sc.next();
+            if (s.length() > 10) {
+                System.out.println(s.charAt(0) + "" + (s.length() - 2) + "" + s.charAt(s.length() - 1));
+            } else {
+                System.out.println(s);
+            }
+        }
+        sc.close();
+    }
+}
