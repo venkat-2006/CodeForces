b287d1dc6cf7c4a3b0e7a4adb4672951f57c1cf2
VENKAT6912 <vibecoder18@gmail.com>
2025-09-30 15:36:34 +0530

    282A

--- 800-1000/Bit.java
+++ 800-1000/Bit.java
@@ -1,6 +1,21 @@
 import java.util.*;
 
-public class Bit {//282A
+public class Bit { // 282A
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int total = 0;
 
-    
+        while (x-- > 0) {
+            String s = sc.next();
+            if (s.equals("++X") || s.equals("X++")) {
+                total++;
+            } else if (s.equals("--X") || s.equals("X--")) {
+                total--;
+            }
+        }
+
+        System.out.println(total);
+        sc.close();
+    }
 }
