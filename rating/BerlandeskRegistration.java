bc75218b9976f5f85e41f99e1db82438fe2dfde3
VENKAT6912 <vibecoder18@gmail.com>
2025-10-07 22:50:26 +0530

    4C

--- rating/BerlandeskRegistration.java
+++ rating/BerlandeskRegistration.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class BerlandeskRegistration {
+public class BerlandeskRegistration {//4c
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
