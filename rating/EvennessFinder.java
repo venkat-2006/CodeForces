b705a9065725be0ea82bc831d2c982c56f8f3c2c
VENKAT6912 <vibecoder18@gmail.com>
2025-10-09 21:24:32 +0530

    25A

---
+++ rating/EvennessFinder.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class EvennessFinder {//25A
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] numbers = new int[n];
+
+       
+        for (int i = 0; i < n; i++) {
+            numbers[i] = sc.nextInt();
+        }
+
+       
+        int[] parities = new int[3];
+        for (int i = 0; i < 3; i++) {
+            parities[i] = numbers[i] % 2;
+        }
+
+        int majorityParity = (parities[0] + parities[1] + parities[2] >= 2) ? 1 : 0;
+
+       
+        for (int i = 0; i < n; i++) {
+            if (numbers[i] % 2 != majorityParity) {
+                System.out.println(i + 1); //
+                break;
+            }
+        }
+
+        sc.close();
+    }
+}
