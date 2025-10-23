561bb3e1d7ef1ee1ffc4cc94aadc29bb32c189ab
VENKAT6912 <vibecoder18@gmail.com>
2025-10-23 16:20:59 +0530

    1903A

---
+++ cp31-800/halloumiBoxes.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class halloumiBoxes {
+    public static void main(String[] args) {//1903A
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = sc.nextInt();
+            }
+
+            if (k == 1) {
+                
+                boolean sorted = true;
+                for (int i = 1; i < n; i++) {
+                    if (a[i] < a[i - 1]) {
+                        sorted = false;
+                        break;
+                    }
+                }
+                System.out.println(sorted ? "YES" : "NO");
+            } else {
+               
+                System.out.println("YES");
+            }
+        }
+        sc.close();
+    }
+}
