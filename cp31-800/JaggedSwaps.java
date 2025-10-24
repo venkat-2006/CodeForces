c724d78bb04e80303cd761a359bb48e755603017
VENKAT6912 <vibecoder18@gmail.com>
2025-10-24 15:05:30 +0530

    1896A

---
+++ cp31-800/JaggedSwaps.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class JaggedSwaps {//1896A
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
+
+            if (isSorted(arr) || arr[0] == 1) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+        sc.close();
+    }
+
+    static boolean isSorted(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i - 1] > arr[i]) return false;
+        }
+        return true;
+    }
+}
