9ce7845eb7082a24f81750badf129916081c7b3d
VENKAT6912 <vibecoder18@gmail.com>
2025-10-29 16:35:28 +0530

    1761A

--- cp31-800/TwoPermutations.java
+++ cp31-800/TwoPermutations.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class TwoPermutations {//1761A
+public class TwoPermutations {//1761A ;)
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int t = scanner.nextInt();
