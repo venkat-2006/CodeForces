eeb9133dfb66b56ce5b1e67b1e8bb7215d40ade4
VENKAT6912 <vibecoder18@gmail.com>
2025-11-03 15:57:46 +0530

    1850D

--- cp31-900/BalancedRound.java
+++ cp31-900/BalancedRound.java
@@ -1,12 +1,33 @@
 import java.util.*;
+
 public class BalancedRound {//1850D
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int x=sc.nextInt();
-        while(x-->0){
-
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+            int[] a = new int[n];
+            
+            for (int i = 0; i < n; i++) {
+                a[i] = sc.nextInt();
+            }
+            
+            Arrays.sort(a);
+            int maxLen = 1, curr = 1;
+            
+            for (int i = 1; i < n; i++) {
+                if (a[i] - a[i - 1] <= k) {
+                    curr++;
+                } else {
+                    maxLen = Math.max(maxLen, curr);
+                    curr = 1;
+                }
+            }
+            maxLen = Math.max(maxLen, curr);
+            System.out.println(n - maxLen);
         }
         sc.close();
     }
-    
 }
