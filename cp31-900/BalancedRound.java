d0cda4b58afa01d7327dcd2b41544aa677bb8dd9
VENKAT6912 <vibecoder18@gmail.com>
2025-11-03 15:55:24 +0530

    1850D

---
+++ cp31-900/BalancedRound.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class BalancedRound {//1850D
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        while(x-->0){
+
+        }
+        sc.close();
+    }
+    
+}
