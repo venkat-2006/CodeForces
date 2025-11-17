60da61879a08babfba38ec0eac292b531e0c33d2
VENKAT6912 <vibecoder18@gmail.com>
2025-11-17 20:24:49 +0530

    1913b

--- cp31-1000/SwapAndDelete.java
+++ cp31-1000/SwapAndDelete.java
@@ -4,25 +4,32 @@ public class SwapAndDelete {//1913B
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
-        
+
         while (t-- > 0) {
             String s = sc.next();
-            int zero = 0, one = 0;
-            
+            int n = s.length();
+
+            int total0 = 0, total1 = 0;
             for (char c : s.toCharArray()) {
-                if (c == '0') zero++;
-                else one++;
+                if (c == '0') total0++;
+                else total1++;
             }
-            
-            for (int i = 0; i < s.length(); i++) {
-                if (s.charAt(i) == '0') zero--;
-                else one--;
-                
-                if (zero == one) {
-                    System.out.println(zero + one);
-                    break;
+
+            int zerosPref = 0, onesPref = 0;
+            int maxKeep = 0;
+
+            for (int i = 0; i < n; i++) {
+                if (s.charAt(i) == '0') zerosPref++;
+                else onesPref++;
+
+                if (onesPref <= total0 && zerosPref <= total1) {
+                    maxKeep = i + 1;
                 }
             }
+
+            System.out.println(n - maxKeep);
         }
+
+        sc.close();
     }
 }
