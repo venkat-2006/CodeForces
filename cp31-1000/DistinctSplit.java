0db7671b3625723a342767426b06bf1cc0446d93
VENKAT6912 <vibecoder18@gmail.com>
2025-12-07 22:04:20 +0530

    1791D

--- cp31-1000/DistinctSplit.java
+++ cp31-1000/DistinctSplit.java
@@ -1,40 +1,40 @@
 import java.util.*;
-
+ 
 public class DistinctSplit { //1791D
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
-
+ 
         while (t-- > 0) {
+            int n = sc.nextInt();
             String s = sc.next();
-            int n = s.length();
-
+ 
             int[] left = new int[n];
             int[] right = new int[n];
-
+ 
             HashSet<Character> set = new HashSet<>();
-
+ 
             for (int i = 0; i < n; i++) {
                 set.add(s.charAt(i));
                 left[i] = set.size();
             }
-
+ 
             set.clear();
-
+ 
             for (int i = n - 1; i >= 0; i--) {
                 set.add(s.charAt(i));
                 right[i] = set.size();
             }
-
+ 
             int ans = 0;
-
+ 
             for (int i = 0; i < n - 1; i++) {
                 ans = Math.max(ans, left[i] + right[i + 1]);
             }
-
+ 
             System.out.println(ans);
         }
-
+ 
         sc.close();
     }
 }
