c9a3303bd251cc449f282ccbba0fc60fcee03d19
VENKAT6912 <vibecoder18@gmail.com>
2026-01-13 23:51:09 +0530

    1821B

--- cp31-1100/SortTheSubarray.java
+++ cp31-1100/SortTheSubarray.java
@@ -1,30 +1,29 @@
 import java.util.*;
 
-public class SortTheSubarray {//1821B
+public class SortTheSubarray { // 1821B
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
 
         while (t-- > 0) {
             int n = sc.nextInt();
-            int[] a = new int[n], b = new int[n];
+            int[] a = new int[n];
+            int[] b = new int[n];
+
             for (int i = 0; i < n; i++) a[i] = sc.nextInt();
             for (int i = 0; i < n; i++) b[i] = sc.nextInt();
 
             int l = 0;
-            while (a[l] == b[l]) l++;
+            while (l < n && a[l] == b[l]) l++;
 
             int r = n - 1;
-            while (a[r] == b[r]) r--;
+            while (r >= 0 && a[r] == b[r]) r--;
 
-            int mn = b[l], mx = b[l];
-            for (int i = l; i <= r; i++) {
-                mn = Math.min(mn, b[i]);
-                mx = Math.max(mx, b[i]);
-            }
+            // expand left in b while sorted continues
+            while (l > 0 && b[l - 1] <= b[l]) l--;
 
-            while (l > 0 && a[l - 1] <= mn) l--;
-            while (r < n - 1 && a[r + 1] >= mx) r++;
+            // expand right in b while sorted continues
+            while (r < n - 1 && b[r] <= b[r + 1]) r++;
 
             System.out.println((l + 1) + " " + (r + 1));
         }
