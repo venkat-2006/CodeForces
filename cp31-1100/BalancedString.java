ed9ff09670d388f0cb2f94163192c4360b7da3ed
VENKAT6912 <vibecoder18@gmail.com>
2026-01-21 18:39:30 +0530

    1673B

---
+++ cp31-1100/BalancedString.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class BalancedString {//1673B
+    public static void main(String args[]){
+     Scanner sc=new Scanner(System.in);
+     int t=sc.nextInt();
+     while(t-->0){
+        String s=sc.nextLine();
+     }
+     sc.close();
+    }
+    
+}
