2b9a08919cad79b5b83eb4b038baa0ecc6ab8239
VENKAT6912 <vibecoder18@gmail.com>
2026-02-08 23:42:03 +0530

    1327A

---
+++ rating/OddIntegers.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class OddIntegers{//1327A
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            long n = sc.nextLong();
+            long k = sc.nextLong();
+
+            if (n >= k * k && (n % 2 == k % 2)) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+    }
+}
