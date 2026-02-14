65b6273e76bf1eb54c0a609850e57d45158516d2
VENKAT6912 <vibecoder18@gmail.com>
2026-02-14 23:46:23 +0530

    2183A

--- rating/BinaryArrayGame.java
+++ rating/BinaryArrayGame.java
@@ -1,35 +1,39 @@
 import java.util.*;
 
-public class BinaryArrayGame { // 2183A
-    
+public class BinaryArrayGame{//2183A
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+
         int t = sc.nextInt();
-        
+
         while (t-- > 0) {
             int n = sc.nextInt();
-            
-            boolean hasZero = false;
-            
+            int[] arr = new int[n];
+
+            int s = 0; 
+
             for (int i = 0; i < n; i++) {
-                int x = sc.nextInt();
-                if (x == 0) {
-                    hasZero = true;
-                }
+                arr[i] = sc.nextInt();
+                if (arr[i] == 1) s++;
             }
-            
-            if (hasZero) {
+
+            if (arr[0] == 0 && arr[n - 1] == 1)
                 System.out.println("Alice");
-            } else {
-                if (n % 2 == 1) {
-                    System.out.println("Alice");
-                } else {
-                    System.out.println("Bob");
-                }
-            }
+
+            else if (arr[0] == 1 && arr[n - 1] == 0)
+                System.out.println("Alice");
+
+            else if (s == n) 
+                System.out.println("Alice");
+
+            else if (arr[0] == 1 && arr[n - 1] == 1 && s < n)
+                System.out.println("Alice");
+
+            else
+                System.out.println("Bob");
         }
-        
+
         sc.close();
     }
 }
