0b090638e26e5b93a7b1a22c00668f637a671aae
VENKAT6912 <vibecoder18@gmail.com>
2026-02-16 23:42:06 +0530

    1539C

--- cp31-1200/StableGroups.java
+++ cp31-1200/StableGroups.java
@@ -1,11 +1,43 @@
 import java.util.*;
-public class StableGroups {
-    public static void main(String agrs[]){
-       Scanner sc=new Scanner(System.in);
-       int t=sc.nextInt();
-       while(t-->0){
 
-       }
-       sc.close();
+public class StableGroups {//1539C
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        long k = sc.nextLong();
+        long x = sc.nextLong();
+
+        long[] a = new long[n];
+        for(int i = 0; i < n; i++){
+            a[i] = sc.nextLong();
+        }
+
+        Arrays.sort(a);
+
+        List<Long> gaps = new ArrayList<>();
+        int groups = 1;
+
+        for(int i = 1; i < n; i++){
+            long diff = a[i] - a[i - 1];
+            if(diff > x){
+                groups++;
+                gaps.add((diff - 1) / x);
+            }
+        }
+
+        Collections.sort(gaps);
+
+        for(long need : gaps){
+            if(k >= need){
+                k -= need;
+                groups--;
+            } else {
+                break;
+            }
+        }
+
+        System.out.println(groups);
+        sc.close();
     }
 }
