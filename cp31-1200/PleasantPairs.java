e6a30e29c2b23767e3ea320785399e046310c7a5
VENKAT6912 <vibecoder18@gmail.com>
2026-03-01 18:15:54 +0530

    1541B

--- cp31-1200/PleasantPairs.java
+++ cp31-1200/PleasantPairs.java
@@ -1,33 +1,38 @@
 import java.util.*;
 
-public class PleasantPairs {//1541B
+public class PleasantPairs {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
 
         while (t-- > 0) {
+            int n = scanner.nextInt();
+            long[][] arr = new long[n][2];
 
-            int n = sc.nextInt();
-            int[] a = new int[n + 1];
-
-            for (int i = 1; i <= n; i++) {
-                a[i] = sc.nextInt();
+            for (int i = 0; i < n; i++) {
+                arr[i][0] = scanner.nextLong();
+                arr[i][1] = i + 1;
             }
 
-            int count = 0;
+            Arrays.sort(arr, (a, b) -> Long.compare(a[0], b[0]));
 
-            for (int i = 1; i <= n; i++) {
-                for (int product = a[i]; product <= 2 * n; product += a[i]) {
-                    int j = product - i;
-                    if (j > i && j <= n && a[i] * a[j] == i + j) {
-                        count++;
+            int ans = 0;
+
+            for (int i = 0; i < n; i++) {
+                for (int j = i + 1; j < n; j++) {
+                    if (arr[i][0] * arr[j][0] > 2L * n) {
+                        break;
+                    }
+
+                    if (arr[i][0] * arr[j][0] == arr[i][1] + arr[j][1]) {
+                        ans++;
                     }
                 }
             }
 
-            System.out.println(count);
+            System.out.println(ans);
         }
 
-        sc.close();
+        scanner.close();
     }
 }
