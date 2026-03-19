418684bdda0190ddf6ba1eb069422b08754b214f
VENKAT6912 <vibecoder18@gmail.com>
2026-03-19 21:44:07 +0530

    1470A

--- cp31-1300/StrangeBirthday.java
+++ cp31-1300/StrangeBirthday.java
@@ -1,20 +1,66 @@
+// import java.util.*;
+
+// public class StrangeBirthday { //1470A
+//     public static void main(String args[]) {
+//         Scanner sc = new Scanner(System.in);
+//         int t = sc.nextInt();
+
+//         while (t-- > 0) {
+
+//             int n = sc.nextInt();
+//             int m = sc.nextInt();
+
+//             int[] k = new int[n];
+//             for (int i = 0; i < n; i++) k[i] = sc.nextInt();
+
+//             long[] c = new long[m];
+//             for (int i = 0; i < m; i++) c[i] = sc.nextLong();
+
+//             Arrays.sort(k);
+
+//             long ans = 0;
+//             int ptr = 0;
+
+//             for (int i = n - 1; i >= 0; i--) {
+//                 if (ptr < k[i]) {
+//                     ans += c[ptr];
+//                     ptr++;
+//                 } else {
+//                     ans += c[k[i] - 1];
+//                 }
+//             }
+
+//             System.out.println(ans);
+//         }
+
+//         sc.close();
+//     }
+// }//TLEEEEEEEEEEEEEEEEEEEEEEEEE
+import java.io.*;
 import java.util.*;
 
-public class StrangeBirthday { //1470A
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
+public class StrangeBirthday {//1470A
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int t = Integer.parseInt(br.readLine());
+
+        StringBuilder output = new StringBuilder();
 
         while (t-- > 0) {
 
-            int n = sc.nextInt();
-            int m = sc.nextInt();
+            st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
 
             int[] k = new int[n];
-            for (int i = 0; i < n; i++) k[i] = sc.nextInt();
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < n; i++) k[i] = Integer.parseInt(st.nextToken());
 
             long[] c = new long[m];
-            for (int i = 0; i < m; i++) c[i] = sc.nextLong();
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < m; i++) c[i] = Long.parseLong(st.nextToken());
 
             Arrays.sort(k);
 
@@ -30,9 +76,9 @@ public class StrangeBirthday { //1470A
                 }
             }
 
-            System.out.println(ans);
+            output.append(ans).append('\n');
         }
 
-        sc.close();
+        System.out.print(output);
     }
 }
