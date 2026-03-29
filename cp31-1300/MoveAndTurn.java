4db91e3c1670df5fa1a6dfaeb070429a179558ad
VENKAT6912 <vibecoder18@gmail.com>
2026-03-29 23:39:05 +0530

    1459B

--- cp31-1300/MoveAndTurn.java
+++ cp31-1300/MoveAndTurn.java
@@ -1,14 +1,18 @@
 import java.util.*;
 
-public class MoveAndTurn { //1459B
+public class MoveAndTurn {//
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        long n = sc.nextLong();
+
+        long k = n / 2;
 
         if (n % 2 == 0) {
-            System.out.println(4);
+            // even case
+            System.out.println((k + 1) * (k + 1));
         } else {
-            System.out.println(4 * n);
+            // odd case
+            System.out.println(2 * (k + 1) * (k + 2));
         }
     }
 }
