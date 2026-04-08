fb11894edf57d5d9d4f9d5a6799e2e99d855ef81
VENKAT6912 <vibecoder18@gmail.com>
2026-04-08 22:21:29 +0530

    706B

---
+++ rating/InterestingDrink.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class InterestingDrink { // 7-6B
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+
+        int x = sc.nextInt();
+
+        for(int i = 0; i < x; i++){
+            int a = sc.nextInt();
+
+            int l = 0;
+            int r = n;
+
+            while(l < r){
+                int mid = (l + r) / 2;
+
+                if(arr[mid] <= a){
+                    l = mid + 1;
+                } else {
+                    r = mid;
+                }
+            }
+
+            System.out.println(l);
+        }
+    }
+}
