7d888f7b3cedfbc747cfbea72837eebeec8b9abf
VENKAT6912 <vibecoder18@gmail.com>
2026-04-12 23:28:59 +0530

    2217A

---
+++ rating/Dungeon.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Dungeon{//2164C
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            int n=sc.nextInt();
+            int m=sc.nextInt();
+
+            int arr1[]=new int[n];
+            int arr2[]=new int[m];
+            int arr3[]=new int[m];
+
+            for(int i=0;i<n;i++){
+                arr1[i]=sc.nextInt();
+            }
+            for(int i=0;i<m;i++){
+                arr2[i]=sc.nextInt();
+            }
+            for(int i=0;i<m;i++){
+                arr3[i]=sc.nextInt();
+            }
+        }
+    }
+
+}
