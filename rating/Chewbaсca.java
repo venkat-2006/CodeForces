df7b10f89363d2d6be5ed147e440c1b43eca19df
VENKAT6912 <vibecoder18@gmail.com>
2026-04-14 23:41:54 +0530

    514A

---
+++ rating/Chewbaсca.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Chewbaсca { // 514A
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String x = sc.next();
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < x.length(); i++) {
+            int digit = x.charAt(i) - '0';
+
+            if (i == 0 && digit == 9) {
+                sb.append(9);
+            } else {
+                int inverted = 9 - digit;
+                sb.append(Math.min(digit, inverted));
+            }
+        }
+
+        System.out.println(sb.toString());
+    }
+}
