b0b9b41ba14ab9d7b9dc1c3af70d61c1652b67aa
VENKAT6912 <vibecoder18@gmail.com>
2026-04-18 16:17:09 +0530

    1463A

--- rating/Dungeon.java
+++ rating/Dungeon.java
@@ -1,26 +1,23 @@
 import java.util.*;
-public class Dungeon{//2164C
-    public static void main(String args[]){
-        Scanner sc=new Scanner(System.in);
-        int t=sc.nextInt();
-        while(t-->0){
-            int n=sc.nextInt();
-            int m=sc.nextInt();
 
-            int arr1[]=new int[n];
-            int arr2[]=new int[m];
-            int arr3[]=new int[m];
+public class Dungeon {//1463A
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
 
-            for(int i=0;i<n;i++){
-                arr1[i]=sc.nextInt();
-            }
-            for(int i=0;i<m;i++){
-                arr2[i]=sc.nextInt();
-            }
-            for(int i=0;i<m;i++){
-                arr3[i]=sc.nextInt();
+        while (t-- > 0) {
+            long a = sc.nextLong();
+            long b = sc.nextLong();
+            long c = sc.nextLong();
+
+            long sum = a + b + c;
+            long max = Math.max(a, Math.max(b, c));
+
+            if (sum % 9 == 0 && max <= sum / 2) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
             }
         }
     }
-
 }
