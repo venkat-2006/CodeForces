140765f2544baa2d7c788a0e6f1f557184433574
VENKAT6912 <vibecoder18@gmail.com>
2026-04-28 22:42:08 +0530

    2164C

--- rating/Dungeon2.java
+++ rating/Dungeon2.java
@@ -11,30 +11,53 @@ public class Dungeon2 {//2164C
             int[] a = new int[n];
             for (int i = 0; i < n; i++) a[i] = sc.nextInt();
 
-            int[][] mon = new int[m][2];
-            for (int i = 0; i < m; i++) mon[i][0] = sc.nextInt();
-            for (int i = 0; i < m; i++) mon[i][1] = sc.nextInt();
+            int[] b = new int[m];
+            int[] c = new int[m];
+
+            for (int i = 0; i < m; i++) b[i] = sc.nextInt();
+            for (int i = 0; i < m; i++) c[i] = sc.nextInt();
 
             Arrays.sort(a);
-            Arrays.sort(mon, (x, y) -> x[0] - y[0]);
 
-            TreeMap<Integer, Integer> map = new TreeMap<>();
-            for (int x : a) map.put(x, map.getOrDefault(x, 0) + 1);
-
-            int ans = 0;
-
+            int[][] mon = new int[m][2];
             for (int i = 0; i < m; i++) {
-                Integer sword = map.ceilingKey(mon[i][0]);
-                if (sword == null) continue;
+                mon[i][0] = b[i];
+                mon[i][1] = c[i];
+            }
 
-                map.put(sword, map.get(sword) - 1);
-                if (map.get(sword) == 0) map.remove(sword);
+            Arrays.sort(mon, (x, y) -> Integer.compare(x[0], y[0]));
+
+            TreeMap<Integer, Integer> swords = new TreeMap<>();
+            for (int x : a) {
+                swords.put(x, swords.getOrDefault(x, 0) + 1);
+            }
+
+            PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+            int i = 0, ans = 0;
+
+            while (!swords.isEmpty()) {
+                int strongest = swords.lastKey();
+
+                while (i < m && mon[i][0] <= strongest) {
+                    pq.add(mon[i][1]);
+                    i++;
+                }
+
+                if (pq.isEmpty()) break;
+
+                int reward = pq.poll();
+
+                int sword = swords.firstKey();
+                int cnt = swords.get(sword);
+                if (cnt == 1) swords.remove(sword);
+                else swords.put(sword, cnt - 1);
 
                 ans++;
 
-                if (mon[i][1] > 0) {
-                    int nw = Math.max(sword, mon[i][1]);
-                    map.put(nw, map.getOrDefault(nw, 0) + 1);
+                if (reward > 0) {
+                    int nw = Math.max(sword, reward);
+                    swords.put(nw, swords.getOrDefault(nw, 0) + 1);
                 }
             }
 
