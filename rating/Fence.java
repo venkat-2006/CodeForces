9ccae95e0c52cfccf2ce4039f18dc10cb9f23d59
VENKAT6912 <vibecoder18@gmail.com>
2026-05-03 23:40:38 +0530

    363B

--- rating/Fence.java
+++ rating/Fence.java
@@ -1,3 +1,39 @@
-public class Fence{//363B
+import java.util.*;
 
+public class Fence { // 363B
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        int[] h = new int[n];
+        for (int i = 0; i < n; i++) {
+            h[i] = sc.nextInt();
+        }
+
+      
+        int sum = 0;
+        for (int i = 0; i < k; i++) {
+            sum += h[i];
+        }
+
+        int minSum = sum;
+        int answerIndex = 0;
+
+        
+        for (int i = k; i < n; i++) {
+            sum = sum - h[i - k] + h[i];
+
+            if (sum < minSum) {
+                minSum = sum;
+                answerIndex = i - k + 1;
+            }
+        }
+
+        System.out.println(answerIndex + 1);
+
+        sc.close();
+    }
 }
