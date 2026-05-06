7ab17186f8ac56839576fe297c8b629a6a0c0c29
VENKAT6912 <vibecoder18@gmail.com>
2026-05-06 23:25:55 +0530

    1736c1

---
+++ rating/GoodSubArrays.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class GoodSubArrays {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int[] a = new int[n + 1];
+
+            for (int i = 1; i <= n; i++) {
+                a[i] = sc.nextInt();
+            }
+
+            long ans = 0;
+            int left = 1;
+
+            for (int r = 1; r <= n; r++) {
+                left = Math.max(left, r - a[r] + 1);
+                ans += (r - left + 1);
+            }
+
+            System.out.println(ans);
+        }
+
+        sc.close();
+    }
+}
