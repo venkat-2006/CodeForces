b55b7fdc10b8e151fbe715f2ae39c4ba9359b789
VENKAT6912 <vibecoder18@gmail.com>
2026-05-12 23:24:30 +0530

    630A

---
+++ rating/AgainTwentyFive.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class AgainTwentyFive {//630A
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.nextLong();
+        System.out.println(25);
+    }
+}
