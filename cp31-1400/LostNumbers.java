1cace87e03e9c8481be9f333be81e2b68bf4d7d7
VENKAT6912 <vibecoder18@gmail.com>
2026-06-08 11:42:00 +0530

    1167B

--- cp31-1400/LostNumbers.java
+++ cp31-1400/LostNumbers.java
@@ -1,68 +1,65 @@
-// import java.util.*;
-
-// public class LostNumbers { // 1167B
-
-//     static int[] nums = {4, 8, 15, 16, 23, 42};
-//     static boolean found = false;
-
-//     static void permute(int idx, int[] arr,
-//                         int p12, int p23, int p34, int p45) {
-
-//         if (found) return;
-
-//         if (idx == arr.length) {
-
-//             if (arr[0] * arr[1] == p12 &&
-//                 arr[1] * arr[2] == p23 &&
-//                 arr[2] * arr[3] == p34 &&
-//                 arr[3] * arr[4] == p45) {
-
-//                 for (int x : arr) {
-//                     System.out.print(x + " ");
-//                 }
-//                 System.out.println();
-
-//                 found = true;
-//             }
-//             return;
-//         }
-
-//         for (int i = idx; i < arr.length; i++) {
-//             swap(arr, idx, i);
-//             permute(idx + 1, arr, p12, p23, p34, p45);
-//             swap(arr, idx, i);
-//         }
-//     }
-
-//     static void swap(int[] arr, int i, int j) {
-//         int temp = arr[i];
-//         arr[i] = arr[j];
-//         arr[j] = temp;
-//     }
-
-//     public static void main(String[] args) {
-
-//         Scanner sc = new Scanner(System.in);
-
-//         int p12 = sc.nextInt();
-//         int p23 = sc.nextInt();
-//         int p34 = sc.nextInt();
-//         int p45 = sc.nextInt();
-
-//         int[] arr = nums.clone();
-
-//         permute(0, arr, p12, p23, p34, p45);
-//     }
-// }
 import java.util.*;
 
-public class LostNumbers {
+public class LostNumbers {//1167B
+
+    static int[] p = {4, 8, 15, 16, 23, 42};
+    static int[] ans = new int[4];
+
+    static boolean nextPermutation(int[] arr) {
+        int n = arr.length;
+
+        int i = n - 2;
+        while (i >= 0 && arr[i] >= arr[i + 1]) i--;
+
+        if (i < 0) return false;
+
+        int j = n - 1;
+        while (arr[j] <= arr[i]) j--;
+
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+
+        int l = i + 1, r = n - 1;
+        while (l < r) {
+            temp = arr[l];
+            arr[l] = arr[r];
+            arr[r] = temp;
+            l++;
+            r--;
+        }
+
+        return true;
+    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        for(int i = 0; i < 6; i++) {
-            System.out.print(sc.nextInt() + " ");
+        for (int i = 0; i < 4; i++) {
+            System.out.println("? " + (i + 1) + " " + (i + 2));
+            System.out.flush();
+            ans[i] = sc.nextInt();
         }
+
+        do {
+            boolean good = true;
+
+            for (int i = 0; i < 4; i++) {
+                if (p[i] * p[i + 1] != ans[i]) {
+                    good = false;
+                    break;
+                }
+            }
+
+            if (good) break;
+
+        } while (nextPermutation(p));
+
+        System.out.print("!");
+        for (int x : p) {
+            System.out.print(" " + x);
+        }
+        System.out.println();
+        System.out.flush();
     }
 }
