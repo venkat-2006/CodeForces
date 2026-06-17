adae7605d30e93b216f5f8eb268a101ab8dbfdbd
VENKAT6912 <vibecoder18@gmail.com>
2026-06-17 18:00:39 +0530

    1891C

--- cp31-1500/SmiloAndMonsters.java
+++ cp31-1500/SmiloAndMonsters.java
@@ -1,32 +1,31 @@
 import java.util.*;
 
-public class SmiloAndMonsters {//////////1891C
+public class SmiloAndMonsters {//1891C
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
-
         while (t-- > 0) {
             int n = sc.nextInt();
             long[] a = new long[n];
+            long sum = 0;
 
             for (int i = 0; i < n; i++) {
                 a[i] = sc.nextLong();
+                sum += a[i];
             }
+
             Arrays.sort(a);
-            int i = 0;
-            int j = n - 1;
-            long sum = 0;
-            long groups = 0;
-            while (i <= j) {
-                if (sum + a[i] <= a[j]) {
-                    sum += a[i];
-                    i++;
-                } else {
-                    groups++;
-                    j--;
-                }
+
+            long rem = sum / 2;
+            long ans = sum - rem;
+
+            for (int i = n - 1; i >= 0 && rem > 0; i--) {
+                rem -= a[i];
+                ans++;
             }
-            System.out.println(groups + sum);
+
+            System.out.println(ans);
         }
 
         sc.close();
