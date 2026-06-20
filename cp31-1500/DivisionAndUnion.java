1d0ac9773986d5e012e3f57bf40a3edae3f473af
VENKAT6912 <vibecoder18@gmail.com>
2026-06-20 23:10:29 +0530

    ,

---
+++ cp31-1500/DivisionAndUnion.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class DivisionAndUnion {//1101C
+
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+           int n=sc.nextInt();
+            int arr[][]=new int[n][2];
+            
+        }
+       
+        sc.close();
+    }
+
+    
+}
