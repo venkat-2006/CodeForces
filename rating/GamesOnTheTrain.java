748e7c53b32b3627c8a00021622bd1e6b64a247c
VENKAT6912 <vibecoder18@gmail.com>
2026-07-14 21:46:35 +0530

    2236A

--- rating/GamesOnTheTrain.java
+++ rating/GamesOnTheTrain.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class GamesOnTheTrain{//2032A
+public class GamesOnTheTrain{//2236A
     public static void main(String[]args){
         Scanner sc=new Scanner(System.in);
         int t=sc.nextInt();
