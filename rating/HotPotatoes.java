fcf450a3e542929861eb2f2790ad84c5419114db
VENKAT6912 <vibecoder18@gmail.com>
2026-08-14 00:51:04 +0530

    2255A

--- rating/HotPotatoes.java
+++ rating/HotPotatoes.java
@@ -9,24 +9,27 @@ public class HotPotatoes { // 2255A
             int n=sc.nextInt();
             long k=sc.nextLong();
             String s=sc.next();
-
             char[] a=s.toCharArray();
+            int N=2*n;
 
-            for(int i=0;i<2*n;i++) {
-                if(a[i]=='1' && a[(i+1)%(2*n)]=='0') {
+            boolean[] move=new boolean[N];
+
+            for(int i=0;i<N;i++)
+                if(a[i]=='1' && a[(i+1)%N]=='0')
+                    move[i]=true;
+
+            for(int i=0;i<N;i++)
+                if(move[i]) {
                     a[i]='0';
-                    a[(i+1)%(2*n)]='1';
+                    a[(i+1)%N]='1';
                 }
-            }
 
             int red=0,blue=0;
-
-            for(int i=0;i<2*n;i++) {
+            for(int i=0;i<N;i++)
                 if(a[i]=='1') {
                     if(i%2==0) blue++;
                     else red++;
                 }
-            }
 
             System.out.println(red+" "+blue);
         }
