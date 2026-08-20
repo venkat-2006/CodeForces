0bee18429a9deb95895a430ffccca7619767efea
VENKAT6912 <vibecoder18@gmail.com>
2026-08-20 21:10:02 +0530

    3069

---
+++ rating/Soln.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Soln {//3069
+    public int[] resultArray(int[] nums) {
+        List<Integer> a=new ArrayList<>();
+        List<Integer> b=new ArrayList<>();
+
+        a.add(nums[0]);
+        b.add(nums[1]);
+
+        for(int i=2;i<nums.length;i++){
+            if(a.get(a.size()-1)>b.get(b.size()-1))
+                a.add(nums[i]);
+            else
+                b.add(nums[i]);
+        }
+
+        int[] res=new int[nums.length];
+        int k=0;
+
+        for(int x:a) res[k++]=x;
+        for(int x:b) res[k++]=x;
+
+        return res;
+    }
+}
